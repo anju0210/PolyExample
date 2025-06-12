9799c7709c5b85e16eea53d48053b79e9e27e78a
anju0210 <s2309@e-mirim.hs.kr>
2025-06-12 15:01:53 +0900

    강아지 Eat, Sleep 메서드 오버라이딩

--- Cat.cs
+++ Cat.cs
@@ -9,7 +9,7 @@ namespace PolyExample
     class Cat : Animal
     {
         public new void Eat() { Console.WriteLine("냐미냐미 먹습니다."); }
-        public new void Sleep() { Console.WriteLine("드르렁 잠을 잡니다."); }
+        public new void Sleep() { Console.WriteLine("소근소근 잠을 잡니다."); }
         public void Meow() { Console.WriteLine("냥냥 웁니다."); }
     }
 }
